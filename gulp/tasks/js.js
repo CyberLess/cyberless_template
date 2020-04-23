03d01026bcef2a1b0271db66e8757a0278e8020f
CyberLess <47046834+CyberLess@users.noreply.github.com>
2020-04-24 02:21:08 +0300

    Обработка ошибок javascript

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -2,40 +2,47 @@
 // Gulp Task: JS
 // =========================================================
 
-let path = require('../settings/path.json'),
-	webpackStream = require('webpack-stream');
+let path = require("../settings/path.json"),
+	webpackStream = require("webpack-stream");
 
 module.exports = (gulp, plugins, browserSync) => {
-    return () => {
-    	var reload = browserSync.reload;
-	    var stream = 
+	return () => {
+		let errorHandler = plugins.notify.onError("<%= error.message %>");
+		var reload = browserSync.reload;
+		var stream =
 			// -------------------------------------------- Start Task
-			    gulp.src(path.src.js)
-		        .pipe(webpackStream({
-		            output: {
-		                filename: 'app.js',
-		            },
-		            module: {
-		                rules: [
-		                    {
-		                        test: /\.(js)$/,
-		                        exclude: /(node_modules)/,
-		                        loader: 'babel-loader',
-		                        query: {
-		                            presets: ['env']
-		                        }
-		                    }
-		                ]
-		            },
-		            externals: {
-		                jquery: 'jQuery'
-		            }
-		        }))
-		        .pipe(gulp.dest(path.build.js))
-		        .pipe(reload({
-		            stream: true
-		        }));
-			// ---------------------------------------------- End Task
-	    return stream;
-    };
+			gulp
+				.src(path.src.js)
+				.pipe(plugins.plumber({ errorHandler }))
+				.pipe(
+					webpackStream({
+						output: {
+							filename: "app.js",
+						},
+						module: {
+							rules: [
+								{
+									test: /\.(js)$/,
+									exclude: /(node_modules)/,
+									loader: "babel-loader",
+									query: {
+										presets: ["env"],
+									},
+								},
+							],
+						},
+						externals: {
+							jquery: "jQuery",
+						},
+					})
+				)
+				.pipe(gulp.dest(path.build.js))
+				.pipe(
+					reload({
+						stream: true,
+					})
+				);
+		// ---------------------------------------------- End Task
+		return stream;
+	};
 };
--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,16 +1,15 @@
 import Inputmask from "inputmask";
-import validate from 'jquery-validation';
+import validate from "jquery-validation";
 import { config } from "../config";
 
 var forms = {
-
 	mask: () => {
 		var selector = document.querySelectorAll("input[name='phone']");
 
 		var im = new Inputmask({
-			"mask": "+7 (999) 999-99-99",
+			mask: "+7 (999) 999-99-99",
 			clearMaskOnLostFocus: true,
-			clearIncomplete: true			
+			clearIncomplete: true,
 		});
 
 		im.mask(selector);
@@ -21,62 +20,61 @@ var forms = {
 			var $form = $(el);
 
 			$form.validate({
-				errorPlacement: function(error, element) { 
-					//just nothing, empty  
+				errorPlacement: function (error, element) {
+					//just nothing, empty
 				},
 				highlight: (element, errorClass, validClass) => {
-					$(element).parent().addClass(errorClass).removeClass(validClass);
+					$(element)
+						.parent()
+						.addClass(errorClass)
+						.removeClass(validClass);
 				},
 				unhighlight: (element, errorClass, validClass) => {
-					$(element).parent().removeClass(errorClass).addClass(validClass);
+					$(element)
+						.parent()
+						.removeClass(errorClass)
+						.addClass(validClass);
 				},
 				submitHandler: (form) => {
-
 					var data = $(form).serialize();
 
 					$.ajax({
-						type: 'POST',
-						url: $(form).attr('action'),
+						type: "POST",
+						url: $(form).attr("action"),
 						data: data,
-						success: function(data) {
-							$(form)[0].reset()
-						}
+						success: function (data) {
+							$(form)[0].reset();
+						},
 					});
-
-				},             
+				},
 				rules: {
-					phone:{
+					phone: {
 						required: true,
 						minlength: 10,
-					},  
-				}		
-			})
-
-		})
+					},
+				},
+			});
+		});
 	},
 
 	events: () => {
-		$('.input__field')
-			.on('focus', (e)=>{
-				let $input = $(e.target)
-				$input.parent().addClass('is-focus')
+		$(".input__field")
+			.on("focus", (e) => {
+				let $input = $(e.target);
+				$input.parent().addClass("is-focus");
 			})
-			.on('blur change', (e)=>{
-				let $input = $(e.target)
+			.on("blur change", (e) => {
+				let $input = $(e.target);
 
-				if($input.val() == '')
-					$input.parent().removeClass('is-focus')
-		 	})		
+				if ($input.val() == "") $input.parent().removeClass("is-focus");
+			});
 	},
 
 	init: () => {
-
 		forms.mask();
 		forms.validate();
 		forms.events();
+	},
+};
 
-	}
-
-}
-
-export {forms};
+export { forms };
